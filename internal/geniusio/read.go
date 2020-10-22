df484d24dd8b8c3cf21928f7337d4c477baf421a
Romain Baran <romain.baran@metronlab.com>
2020-10-22 18:14:51 +0200

    Add standard input read

--- internal/geniusio/read.go
+++ internal/geniusio/read.go
@@ -1,10 +1,13 @@
 package geniusio
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	"github.com/BurntSushi/toml"
@@ -13,8 +16,21 @@ import (
 
 var ErrUnsupportedEncoding = errors.New("unsupported encoding")
 
+func ReadStdIn() (interface{}, error) {
+	reader := bufio.NewReader(os.Stdin)
+	data, err := reader.ReadString(0)
+	if errors.Is(err, io.EOF) {
+		return data, nil
+	}
+	return data, err
+}
+
 //ReadFileData decode toml, yaml or json based on file extension
 func ReadFileData(path string) (interface{}, error) {
+	if path == "" {
+		return ReadStdIn()
+	}
+
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
