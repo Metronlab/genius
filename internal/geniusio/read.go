38a8e33fbd6168516007992be6ce613158365a2f
nyhu <toussaint.boos@metronlab.com>
2020-10-22 20:23:50 +0200

    can specify input format from stdin or force format on data file

--- internal/geniusio/read.go
+++ internal/geniusio/read.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 
 	"github.com/BurntSushi/toml"
 	"gopkg.in/yaml.v2"
@@ -16,43 +17,66 @@ import (
 
 var ErrUnsupportedEncoding = errors.New("unsupported encoding")
 
-func ReadStdIn() (interface{}, error) {
-	reader := bufio.NewReader(os.Stdin)
-	data, err := reader.ReadString(0)
-	if errors.Is(err, io.EOF) {
-		return data, nil
-	}
-	return data, err
-}
-
 //ReadFileData decode toml, yaml or json based on file extension
-func ReadFileData(path string) (interface{}, error) {
+func ReadFileData(path, format string) (interface{}, error) {
+	var unmarshal func(data []byte, v interface{}) error
+	var err error
 	if path == "" {
-		return ReadStdIn()
+		if format == "" {
+			format = "txt"
+		}
+		unmarshal, err = makeUnmarshaller(fmt.Sprintf(".%s", format))
+	} else {
+		unmarshal, err = makeUnmarshaller(filepath.Ext(path))
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if path == "" {
+		return readStdIn(unmarshal)
 	}
 
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
+
 	var v interface{}
-
-	switch ext := filepath.Ext(path); ext {
-	case ".json", ".JSON":
-		if err := json.Unmarshal(data, &v); err != nil {
-			return nil, fmt.Errorf("invalid JSON data: %w", err)
-		}
-	case ".yaml", ".YAML", ".yml", ".YML":
-		if err := yaml.Unmarshal(data, &v); err != nil {
-			return nil, fmt.Errorf("invalid YAML data: %w", err)
-		}
-	case ".toml", ".TOML":
-		if err := toml.Unmarshal(data, &v); err != nil {
-			return nil, fmt.Errorf("invalid TOML data: %w", err)
-		}
-	default:
-		return nil, fmt.Errorf("decoding file with extension %s: %w", ext, ErrUnsupportedEncoding)
+	if err := unmarshal(data, &v); err != nil {
+		return nil, fmt.Errorf("invalid data: %w", err)
 	}
-
 	return v, nil
+}
+
+func readStdIn(unmarshall func(data []byte, v interface{}) error) (v interface{}, err error) {
+	reader := bufio.NewReader(os.Stdin)
+	data, err := reader.ReadBytes(0)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return nil, err
+	}
+	err = unmarshall(data, &v)
+	return
+}
+
+func makeUnmarshaller(extension string) (func(data []byte, v interface{}) error, error) {
+	switch extension {
+	case ".json", ".JSON":
+		return json.Unmarshal, nil
+	case ".yaml", ".YAML", ".yml", ".YML":
+		return yaml.Unmarshal, nil
+	case ".toml", ".TOML":
+		return toml.Unmarshal, nil
+	case ".txt", ".text":
+		return func(data []byte, v interface{}) error {
+			rv := reflect.ValueOf(v)
+			if rv.Kind() != reflect.Ptr || rv.IsNil() {
+				panic("only accept interface{}")
+			}
+			rv.Elem().Set(reflect.ValueOf(string(data)))
+			return nil
+		}, nil
+	default:
+		return nil, fmt.Errorf("decoding file with format \"%s\": %w", extension, ErrUnsupportedEncoding)
+	}
 }
